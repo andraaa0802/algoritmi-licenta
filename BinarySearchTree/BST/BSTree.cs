daba661f1b7caa384eb6001d9105f1763c3795e1
andraaa0802 <92014452+andraaa0802@users.noreply.github.com>
2024-06-17 17:22:14 +0300

    Add files via upload

---
+++ BinarySearchTree/BST/BSTree.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    internal class BSTree : ITree
+    {
+        public Node Root { get; set; } = null;
+
+        public void Delete(Node node)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void InorderTreeWalk()
+        {
+            InorderTreeWalkHelper(Root);
+        }
+
+        private void InorderTreeWalkHelper(Node p)
+        {
+            if(p != null)
+            {
+                InorderTreeWalkHelper(p.Left);
+                Console.WriteLine($"{p.Key} -> ");
+                InorderTreeWalkHelper(p.Right);
+            }
+        }
+
+        public void Insert(Node node)
+        {
+            Node y = null;
+            Node x = Root;
+            while(x != null)
+            {
+                y = x;
+                if (node.Key < x.Key)
+                    x = x.Left;
+                else if (node.Key > x.Key)
+                    x = x.Right;
+                else
+                {
+                    x.Frequency += 1;
+                    return;
+                }
+            }
+            node.Parent = y;
+            node.Frequency = 1;
+            if (y == null)
+                Root = node;
+            else if (node.Key < y.Key)
+                y.Left = node;
+            else
+                y.Right = node;
+        }
+
+        public void PostorderTreeWalk()
+        {
+            PostorderTreeWalkHelper(Root);
+        }
+
+        private void PostorderTreeWalkHelper(Node p)
+        {
+            if (p != null)
+            {
+                PostorderTreeWalkHelper(p.Left);
+                PostorderTreeWalkHelper(p.Right);
+                Console.WriteLine($"{p.Key} -> ");
+            }
+        }
+
+        public void PreorderTreeWalk()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Node Search(int key)
+        {
+            Node x = Root;
+            while(x != null && x.Key != key)
+            {
+                if (key < x.Key)
+                    x = x.Left;
+                else
+                    x = x.Right;
+            }
+            return x;
+        }
+    }
+}
---
+++ BinarySearchTree/BST/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BST
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BSTree T = new BSTree();
+            List<Node> lst = new List<Node>
+            {
+                new Node{ Key = 5  },
+                new Node{ Key = 3  },
+                new Node{ Key = 10  },
+                new Node{ Key = 4  },
+                new Node{ Key = 1  },
+                new Node{ Key = 15  },
+                new Node{ Key = 7  }
+            };
+            foreach (Node node in lst)
+            {
+                T.Insert(node);
+            }
+            T.PostorderTreeWalk();
+        }
+    }
+}
