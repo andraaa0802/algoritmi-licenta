a9e7cbe862328b46f3fa7d194043578b75bd826d
andraaa0802 <92014452+andraaa0802@users.noreply.github.com>
2024-06-17 17:14:45 +0300

    Add files via upload

---
+++ Stiva/0306/MyStack.cs
@@ -0,0 +1,84 @@
+﻿using System.Diagnostics;
+
+public class MyStack<T> : IStack<T>
+{
+    private T[] data;
+    private int capacity = 16;
+    private int size = 0;
+    /// <summary>
+    /// Constructor implicit
+    /// </summary>
+    public MyStack()
+    {
+        Console.Write($"Creating new stack with capacity {capacity}...");
+        data = new T[capacity];
+        Console.WriteLine("success!");
+    }
+    /// <summary>
+    /// Intoarce si elimina elementul din varful stivei
+    /// </summary>
+    /// <returns></returns>
+    /// <exception cref="StackEmptyException"></exception>
+    public T Pop()
+    {
+        Console.WriteLine("Trying to Pop the stack...");
+        if (size > 0)
+        {
+            T ret = data[size - 1];
+            if (size == capacity / 4)
+                resize(capacity / 2);
+            Console.WriteLine("success!");
+            size--;
+            return ret;
+        }
+        else
+            throw new StackEmptyException("A fost apelata functia Pop pe o stiva goala");
+    }
+    /// <summary>
+    /// Adauga o noua valoare in stiva
+    /// </summary>
+    /// <param name="value">Valoarea care se adauga</param>
+    public void Push(T value)
+    {
+        Console.WriteLine($"Trying to push {value} into the stack...");
+        if (Count == capacity)
+        {
+            resize(2 * capacity);
+        }
+        data[size++] = value;
+        Console.WriteLine("success!");
+    }
+
+    private void resize(int newCapacity)
+    {
+        Console.Write($"Resizing stack: old capacity = {capacity}, new capacity = {newCapacity}...");
+        T[] newdata = new T[newCapacity];
+        for (int i = 0; i < Count; i++)
+        {
+            newdata[i] = data[i];
+        }
+        capacity = newCapacity;
+        data = newdata;
+        Console.WriteLine("success");
+    }
+
+    public T Peek()
+    {
+        Console.Write("Try to peek into the stack...");
+        if (size > 0)
+        {
+            Console.WriteLine("success");
+            return data[size - 1];
+        }
+        else
+            throw new StackEmptyException("A fost apelata functia Peek pe o stiva goala");
+    }
+    //public int Count 
+    //{ 
+    //    get
+    //    {
+    //        return size;
+    //    }
+    //}
+    public int Count => size;
+}
---
+++ Stiva/0306/Program.cs
@@ -0,0 +1,75 @@
+﻿/*Stack<int> st =  new Stack<int>();
+int n = 10;
+for (int i = 0; i < n; i++)
+    st.Push(i);
+
+Console.WriteLine($"Elementul din varful stivei este: {st.Peek()}");
+
+while (st.Count > 0)
+{
+    Console.WriteLine(st.Pop());
+}
+*/
+
+MyStack<int> ms = new MyStack<int>();
+
+ms.Push(5);
+ms.Push(6);
+ms.Push(7);
+
+Console.WriteLine($"Elementul din varful stivei este: {ms.Peek()}");
+
+while(ms.Count > 0)
+{
+    Console.WriteLine(ms.Pop());
+}
+
+// Adaug 100 de elemente in stiva
+for (int i = 0; i < 100; i++)
+    ms.Push(i);
+
+
+try
+{
+	// Incerc sa elimin 101 elemente din stiva
+	for (int i = 0; i <= 100; i++)
+		Console.WriteLine(ms.Pop());
+}
+catch (StackEmptyException e)
+{
+    Console.WriteLine(e.Message);
+}
+
+
+MyStack<string> mss = new MyStack<string>();
+mss.Push("hello");
+mss.Push("world");
+
+//Console.WriteLine(mss.Pop());
+//Console.WriteLine(mss.Pop());
+F(mss);
+
+StackLL<int> sll = new StackLL<int>();
+sll.Push(5);
+sll.Push(6);
+sll.Push(7);
+
+
+F<int>(sll);
+
+while(sll.Count > 0)
+{
+    Console.WriteLine(sll.Pop());
+}
+
+
+
+
+Console.ReadKey();
+
+static void F<T>(IStack<T> stack)
+{
+    Console.WriteLine("Entering F<T>()");
+    Console.WriteLine(stack.Peek());
+    Console.WriteLine("Exiting F<T>()");
+}
